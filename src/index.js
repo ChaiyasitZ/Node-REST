c225d5f8246f8c4662e89ab808ece4682b835587
Chaiyasit Mintakorn <s6506022610014@email.kmutnb.ac.th>
2023-09-12 10:32:57 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,78 @@
-require("dotenv").config(); 
+require('dotenv').config();
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app = express();
 
-const express = require("express"); 
-const app = express(); 
-const port = process.env.PORT || 3000; 
- 
-app.get("/", (req, res) => { 
-  res.send("Hello World!"); });
+const db = new sqlite3.Database('./Database/Book.sqlite');
 
-app.listen(port, () => { 
-  console.log(`Example app listening at http://localhost:${port}`);});
+app.use(express.json());
+
+db.run(`CREATE TABLE IF NOT EXISTS Book (
+    id INTEGER PRIMARY KEY, 
+    title TEXT,
+    author TEXT
+)`);
+
+app.get('/books', (req, res) => {
+    db.all('SELECT * FROM Book', (err, row) => {
+        if (err) {
+            res.Status(500).send(err);
+        } else {
+            res.json(row);
+        }
+    });
+});
+
+app.get('/books/:id', (req, res) => {
+    db.get('SELECT * FROM Book WHERE id = ?', req.params.id, (err, row) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (!row) {
+                res.status(404).send('Book Not Found');
+            } else {
+                res.json(row);
+            }
+        }
+    });
+});
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+    db.run(`INSERT INTO Book (title, author) VALUES (?, ?)`, book.title, book.author, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            book.id = this.lastID;
+            res.send(book);
+        }
+    });
+});
+
+app.put('/books/:id', (req, res) => {
+    const book = req.body;
+    db.run(`UPDATE Book SET title = ?, author = ? WHERE id = ?`, book.title, book.author, req.params.id, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(book);
+        }
+    });
+});
+
+app.delete('/books/:id', (req, res) => {
+    db.run(`DELETE FROM Book WHERE id = ?`, req.params.id, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else if (this.changes === 0) {
+            res.status(404).send('Book Not Found');
+        } 
+        else {
+            res.send('Book is deleted');
+        }
+    });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
