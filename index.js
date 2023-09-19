e327476b3a5e0406de948cff0d647edfd9f10d85
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-09-19 20:20:25 +0700

    update

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
