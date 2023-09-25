280e9147a2dc7dabdbb1ad26a3e34a719964a8f4
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-09-25 22:34:50 +0700

    update

--- PGSqlCRUD.js
+++ PGSqlCRUD.js
@@ -4,5 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-const dbUrl = 'postgres://postgres:password@localhost:5432/PGBooks';
+const dbUrl = Sequelize('postgres://webadmin:YKKgle22587@node51817-chaiaysitz.proen.app.ruk-com.cloud:11492/Books');
+
 const sequelize = new Sequelize(dbUrl);
