e1246dc6fa0085ac6325357913d9c6ca082455cd
Chaiyasit Mintakorn <s6506022610014@email.kmutnb.ac.th>
2023-09-26 10:41:21 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const dbUrl = 'postgres://webadmin:YKKgle22587@node51817-chaiaysitz.proen.app.ruk-com.cloud:11492/Books'
+const dbUrl = 'postgres://webadmin:YKKgle22587@10.104.15.125:5432/Books'
 
 const sequelize = new Sequelize(dbUrl);
 
